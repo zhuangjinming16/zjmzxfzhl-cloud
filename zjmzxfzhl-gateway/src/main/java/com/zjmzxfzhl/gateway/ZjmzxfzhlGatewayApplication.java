4ec46dc1905119245de3b1ba78a220640980a845
zjm16@163.com <zjm16@163.com>
2020-08-13 00:29:16 +0800

    修改Sentinel配置网关流控模式,且支持自定义API分组

--- zjmzxfzhl-gateway/src/main/java/com/zjmzxfzhl/gateway/ZjmzxfzhlGatewayApplication.java
+++ zjmzxfzhl-gateway/src/main/java/com/zjmzxfzhl/gateway/ZjmzxfzhlGatewayApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ZjmzxfzhlGatewayApplication {
     public static void main(String[] args) {
+        System.setProperty("csp.sentinel.app.type", "1");
         SpringApplication.run(ZjmzxfzhlGatewayApplication.class, args);
     }
 }
